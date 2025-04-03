ddbe26b64f8a2da8fadc65a6a49020ec705f2b1c
alexx-kis <kislov@advline.ru>
2025-04-03 12:07:07 +0300

    modify picture

--- src/components/ui/picture/picture.tsx
+++ src/components/ui/picture/picture.tsx
@@ -5,47 +5,56 @@ import './picture.scss';
 
 // ^======================== Picture ========================^ //
 
+type WithWidthHeight = {
+  width: number;
+  height: number;
+  size?: never;
+};
+
+type WithSize = {
+  size: [number] | [number, number];
+  width?: never;
+  height?: never;
+};
+
 type PictureProps = {
   bemClass?: string;
   imgClass?: string;
   src: string;
   sources: string[];
   alt?: string;
-  width: number;
-  height: number;
   ref?: RefObject<HTMLElement | null>;
-};
+} & (WithWidthHeight | WithSize);
 
 function Picture(pictureProps: PictureProps): React.JSX.Element {
+  const { src, alt = '', bemClass, imgClass, sources, ref } = pictureProps;
 
-  const { src, alt, width, height, bemClass, imgClass, sources, ref } = pictureProps;
+  let width: number = 0;
+  let height: number = 0;
+
+  if ('width' in pictureProps && 'height' in pictureProps) {
+    width = pictureProps.width as number;
+    height = pictureProps.height as number;
+  } else if ('size' in pictureProps) {
+    if (pictureProps.size.length === 1) {
+      width = pictureProps.size[0];
+      height = pictureProps.size[0];
+    } else if (pictureProps.size.length === 2) {
+      [width, height] = pictureProps.size;
+    }
+  } else {
+    throw new Error("Either 'width' and 'height' or 'size' must be provided.");
+  }
 
   return (
-    <picture
-      className={bemClass}
-      ref={ref}
-    >
-      {
-        sources.map((source) =>
-          <source key={source}
-            srcSet={source}
-            type={`image/${extractExtension(source)}`}
-          />
-        )
-      }
-      <source
-        key={src}
-        srcSet={src}
-        type={`image/${extractExtension(src)}`}
-      />
-      <Image
-        src={src}
-        alt={alt ? alt : ''}
-        width={width}
-        height={height}
-        className={imgClass}
-      />
+    <picture className={bemClass} ref={ref}>
+      {sources.map((source) => (
+        <source key={source} srcSet={source} type={`image/${extractExtension(source)}`} />
+      ))}
+      <source key={src} srcSet={src} type={`image/${extractExtension(src)}`} />
+      <Image src={src} alt={alt} width={width} height={height} className={imgClass} />
     </picture>
   );
 }
+
 export default Picture;
