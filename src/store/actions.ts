cd398ca94890e29b01287a168bc05d1d9c009287
alexx-kis <kislov@advline.ru>
2025-05-14 09:37:17 +0300

    remove actions - they are in slice now

--- src/store/actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-// %======================== actions ========================% //
-
-export const doSomethingAction = createAction<string>('domain/doSomething');
-
-// string - type of what will be passed as payload
